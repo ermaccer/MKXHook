7d8298ea8d8288c0621b598b400b440cb285d545
ermaccer <40604575+ermaccer@users.noreply.github.com>
2020-08-07 14:44:55 +0200

    Add files via upload

--- source/code/eSettingsManager.cpp
+++ source/code/eSettingsManager.cpp
@@ -11,10 +11,23 @@ void eSettingsManager::Init()
 	bDisableAssetHashChecking = ini.ReadBoolean("Settings", "bDisableAssetHashChecking", false);
 	bEnableNPCFatalities =  ini.ReadBoolean("Settings", "bEnableNPCFatalities", false);
 	bFixNPCGenderFatalityMessage = ini.ReadBoolean("Settings", "bFixNPCGenderFatalityMessage", false);
-	bEnable60FPSFrontend = ini.ReadBoolean("Settings", "bEnable60FPSFrontend", false);
+	bEnable60FPSFrontend = ini.ReadBoolean("Settings", "bEnable60FPSPatch", false);
 
 	iHookMenuOpenKey = ini.ReadInteger("Settings", "iHookMenuOpenKey", VK_F1);
 	iAvailableDLCCells = ini.ReadInteger("Settings", "iAvailableDLCCells", -1);
-	
+
+	iFreeCameraKeyXPlus = ini.ReadInteger("Settings", "iFreeCameraKeyXPlus", 0);
+	iFreeCameraKeyXMinus = ini.ReadInteger("Settings", "iFreeCameraKeyXMinus", 0);
+
+
+	iFreeCameraKeyYPlus = ini.ReadInteger("Settings", "iFreeCameraKeyYPlus", 0);
+	iFreeCameraKeyYMinus = ini.ReadInteger("Settings", "iFreeCameraKeyYMinus", 0);
+
+	iFreeCameraKeyZPlus = ini.ReadInteger("Settings", "iFreeCameraKeyZPlus", 0);
+	iFreeCameraKeyZMinus = ini.ReadInteger("Settings", "iFreeCameraKeyZMinus", 0);
+
+
+	iFreeCameraKeyYawPlus = ini.ReadInteger("Settings", "iFreeCameraKeyYawPlus", 0);
+	iFreeCameraKeyYawMinus = ini.ReadInteger("Settings", "iFreeCameraKeyYawMinus", 0);
 	//bDisableComboDamageScaling = ini.ReadBoolean("Settings",)
 }
